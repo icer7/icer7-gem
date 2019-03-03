5c7dbea5d2c1fd1c4c3fcc26e490278d72d319cb
ryo <ryo0104777@gmail.com>
2019-03-03 16:47:06 +0900

    first

---
+++ lib/icer7.rb
@@ -0,0 +1,6 @@
+require "icer7/version"
+
+module Icer7
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/icer7_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Icer7 do
+  it "has a version number" do
+    expect(Icer7::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
